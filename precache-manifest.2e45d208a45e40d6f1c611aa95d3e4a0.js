8f09dd161913f29344edae3048b3855db51a5d6d
Maiduydung <maiduydung@yahoo.com>
2020-03-14 23:01:45 +0900

    Updates

---
+++ precache-manifest.2e45d208a45e40d6f1c611aa95d3e4a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8083019f8c560f79136885cba67a8817",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "dab30d0e2a4268b05df4",
+    "url": "/robots/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "8ed45c7ccbd09f3f2640",
+    "url": "/robots/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dab30d0e2a4268b05df4",
+    "url": "/robots/static/js/2.1a94f78b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robots/static/js/2.1a94f78b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed45c7ccbd09f3f2640",
+    "url": "/robots/static/js/main.28677b1a.chunk.js"
+  },
+  {
+    "revision": "a84c21783c6891679228",
+    "url": "/robots/static/js/runtime-main.e075c96b.js"
+  }
+]);
